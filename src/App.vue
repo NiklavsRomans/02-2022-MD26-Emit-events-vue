cba1ae81fe4e4c9c78c7632e89fcc7135be73585
Niklavs Romans <niklavsromans19@gmail.com>
2022-06-06 04:00:39 +0200

    mine files

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <section class="main-view">
+          <InputBox01 />
+          <InputBox02 />
+          <InputBox03 />
+          <InputBox04 />
+          <DescriptionBox01 />
+          <DescriptionBox02 />
+          <ToggleBox01 />
+          <ToggleBox02 />
+        </section>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
+//TASK ONE IMPORTS
+import InputBox01 from "./components/taskone/InputBox01.vue";
+import InputBox02 from "./components/taskone/InputBox02.vue";
+import InputBox03 from "./components/taskone/InputBox03.vue";
+import InputBox04 from "./components/taskone/InputBox04.vue";
+
+// TASK TWO IMPORTS
+import DescriptionBox01 from "./components/tasktwo/DescriptionBox01.vue";
+import DescriptionBox02 from "./components/tasktwo/DescriptionBox02.vue";
+import ToggleBox01 from "./components/tasktwo/ToggleBox01.vue";
+import ToggleBox02 from "./components/tasktwo/ToggleBox02.vue";
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    InputBox01,
+    InputBox02,
+    InputBox03,
+    InputBox04,
+    DescriptionBox01,
+    DescriptionBox02,
+    ToggleBox01,
+    ToggleBox02,
+  },
+  data: () => {
+    return {};
   },
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-view {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 50px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./styles/base.scss";
+import "./styles/flexboxgrid.scss";
+import "./styles/cssReset.scss";
 
 createApp(App).mount("#app");
